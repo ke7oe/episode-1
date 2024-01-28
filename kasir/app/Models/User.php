691e4346f5106d5ff5a2d54d214852d757ca9cbd
ke7oe <157286818+ke7oe@users.noreply.github.com>
2024-01-28 23:26:12 +0700

    episode-2
    Migration

---
+++ kasir/app/Models/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+    const CREATED_AT = 'creation_date';
+    const UPDATED_AT = 'updated_date';
+}
